29d6c24b26fa9ee05839c9740f65178c16e350c9
a7x <rohit.dholakia@gmail.com>
2013-06-04 22:52:41 -0700

    removed that annoying ds store

--- Scripts/CountingUsers.py
+++ Scripts/CountingUsers.py
@@ -2,24 +2,20 @@
 import sys
 import os
 from lxml import etree
-
-import codecs
-import marshal
+from Utils import *
 if(len(sys.argv)<2):
 	print 'python CountingUsers.py xmlTopDirectory outputfile'
 
-output = open(sys.argv[2],'w')
+output = open(sys.argv[2], 'w')
 for l in os.listdir(sys.argv[1]):
 	if('.DS_Store' not in l):
 		userCount = 0
-		infile = open(os.path.join(sys.argv[1],l,'users.xml'))
+		infile = open(os.path.join(sys.argv[1], l, 'users.xml'))
 		context = etree.iterparse(infile)
-		for event,elem in context:
-			userCount+=1
-			elem.clear()
-			while elem.getprevious() is not None:
-				del elem.getparent()[0]
-		output.write(l+'\t'+str(userCount)+'\n')
+		for event, elem in context:
+			userCount += 1
+			clearElem(elem)
+		output.write(l + '\t' + str(userCount) + '\n')
 
 
 
